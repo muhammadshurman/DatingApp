311a5ec21e26f5ce7e3cd2c93a03f8e75c090617
Muhammad Al-Shurman <108474043+muhammadshurman@users.noreply.github.com>
2024-01-18 16:58:00 +0300

    v1-finished

---
+++ API/Controllers/UsersController.cs
@@ -0,0 +1,38 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using API.Data;
+using API.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class UsersController
+    {
+        private readonly DataContext _dbContext;
+
+        public UsersController(DataContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<AppUser>>> GetAllUsers()
+        {
+            var users = await _dbContext.Users.ToListAsync();
+            return users;
+        }
+
+        [HttpGet]
+        [Route("{id}")]
+        public async Task<ActionResult<AppUser>> GetUserById(int id)
+        {
+            return await _dbContext.Users.FindAsync(id);
+
+        }
+    }
+}
